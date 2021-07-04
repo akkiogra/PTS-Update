7081f2e2fe0970330e999a088a73ec6d2d04fc74
Xployt <43792836+Xpl0yt91@users.noreply.github.com>
2021-07-04 22:25:56 +0100

    Update.sh
    Repo changes and tidy up.

--- update.sh
+++ update.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 #
-# Title:      PTS major file
+# Title:      Pandaura major file
 # org.Author(s):  Admin9705 - Deiteq
 # Mod from MrDoob for PTS
 # GNU:        General Public License v3.0
 ################################################################################
 
 sudocheck() {
-  if [[ $EUID -ne 0 ]]; then
-printf '
+    if [[ $EUID -ne 0 ]]; then
+        printf '
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ⛔️  You Must Execute as a SUDO USER (with sudo) or as ROOT!
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-'
-    exit 0
-  fi
+        '
+        exit 0
+    fi
 }
 
 base() {
-rm -rf /opt/pgstage && mkdir -p /opt/pgstage 1>/dev/null 2>&1
-ansible-playbook /opt/ptsupdate/stage/pgstage.yml 1>/dev/null 2>&1
+    rm -rf /opt/pgstage && mkdir -p /opt/pgstage 1>/dev/null 2>&1
+    ansible-playbook /opt/ptsupdate/stage/pgstage.yml 1>/dev/null 2>&1
 }
 
 mainstart() {
-  file="/opt/pgstage/place.holder"
-  waitvar=0
-  while [ "$waitvar" == "0" ]; do
-    sleep .5
-    if [ -e "$file" ]; then waitvar=1; fi
-  done
-
-  pgnumber=$(cat /var/plexguide/pg.number)
-  versions=$(cat /opt/pgstage/versions.sh)
-  release="$(curl -s https://api.github.com/repos/PTS-Team/PTS-Team/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
-
+    file="/opt/pgstage/place.holder"
+    waitvar=0
+    while [ "$waitvar" == "0" ]; do
+        sleep .5
+        if [ -e "$file" ]; then waitvar=1; fi
+    done
+    
+    pgnumber=$(cat /var/plexguide/pg.number)
+    versions=$(cat /opt/pgstage/versions.sh)
+    release="$(curl -s https://api.github.com/repos/Pandaura/PTS-Team/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+    
   tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -49,32 +49,32 @@ Installed : $pgnumber
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
-
-  break=no
-  read -p '🌍  TYPE Version | PRESS ENTER: ' typed
-  storage=$(grep $typed /opt/pgstage/versions.sh)
-  parttwo
+    
+    break=no
+    read -p '🌍  TYPE Version | PRESS ENTER: ' typed
+    storage=$(grep $typed /opt/pgstage/versions.sh)
+    parttwo
 }
 
 parttwo() {
-  if [[ "$typed" == "exit" || "$typed" == "Exit" || "$typed" == "EXIT" || "$typed" == "z" || "$typed" == "Z" ]]; then
-    echo ""
-    touch /var/plexguide/exited.upgrade
-    exit
-  fi
-  if [ "$storage" != "" ]; then
-    break=yes
-    echo -e $storage >/var/plexguide/pg.number
-    ansible-playbook /opt/ptsupdate/version/choice.yml
+    if [[ "$typed" == "exit" || "$typed" == "Exit" || "$typed" == "EXIT" || "$typed" == "z" || "$typed" == "Z" ]]; then
+        echo ""
+        touch /var/plexguide/exited.upgrade
+        exit
+    fi
+    if [ "$storage" != "" ]; then
+        break=yes
+        echo -e $storage >/var/plexguide/pg.number
+        ansible-playbook /opt/ptsupdate/version/choice.yml
 		tee <<-EOF
 
 		━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 		✅️  SYSTEM MESSAGE: Installing Version - $typed - Standby!
 		━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 		EOF
-		sleep 2
+        sleep 2
         file="/var/plexguide/community.app"
-		if [ -e "$file" ]; then rm -rf /var/plexguide/community.app; fi
+        if [ -e "$file" ]; then rm -rf /var/plexguide/community.app; fi
     else
 		tee <<-EOF
 
@@ -82,64 +82,64 @@ parttwo() {
 		⛔️  SYSTEM MESSAGE: Version $typed does not exist! - Standby!
 		━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 		EOF
-		sleep 2
-    mainstart
-  fi
+        sleep 2
+        mainstart
+    fi
 }
 
 ende() {
-alias 1>/dev/null 2>&1
-remove 1>/dev/null 2>&1
-redit 1>/dev/null 2>&1
-owned 1>/dev/null 2>&1
-cleartabs
-check
+    alias 1>/dev/null 2>&1
+    remove 1>/dev/null 2>&1
+    redit 1>/dev/null 2>&1
+    owned 1>/dev/null 2>&1
+    cleartabs
+    check
 }
 
 
 ########end funtions // execute commands
 
 badinput() {
-  echo
-  read -p '⛔️ ERROR - Bad Input! | Press [ENTER] ' typed </dev/tty
+    echo
+    read -p '⛔️ ERROR - Bad Input! | Press [ENTER] ' typed </dev/tty
 }
 
 alias() {
-  ansible-playbook /opt/plexguide/menu/alias/alias.yml
+    ansible-playbook /opt/plexguide/menu/alias/alias.yml
 }
 
 remove() {
-  ansible-playbook /opt/plexguide/menu/pg.yml --tag remove
+    ansible-playbook /opt/plexguide/menu/pg.yml --tag remove
 }
 
 cleartabs() {
-truncate -s 0 /var/plexguide/logs/*
+    truncate -s 0 /var/plexguide/logs/*
 }
 
 redit() {
-canonical-livepatch disable
-disable-livepatch -r
+    canonical-livepatch disable
+    disable-livepatch -r
 }
 
 owned() {
-  chown -cR 1000:1000 /opt/plexguide
-  chmod -R 775 /opt/plexguide
+    chown -cR 1000:1000 /opt/plexguide
+    chmod -R 775 /opt/plexguide
 }
 
 check() {
- bash /opt/plexguide/menu/interface/ending.sh 
-file="/opt/plexguide/menu/pg.yml"
-  if [[ -f $file ]]; then
-printf '
+    bash /opt/plexguide/menu/interface/ending.sh
+    file="/opt/plexguide/menu/pg.yml"
+    if [[ -f $file ]]; then
+        printf '
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-✅ All files Valid and > PTS is up to date <
+✅ All files Valid and > Pandaura is up to date <
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-'
- printf '
+        '
+        printf '
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-✅ All files Valid and > PTS is up to date <
+✅ All files Valid and > Pandaura is up to date <
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-' >>/var/plexguide/logs/pg.log
- else ansible-playbook /opt/plexguide/menu/version/missing_pull.yml; fi
+        ' >>/var/plexguide/logs/pg.log
+else ansible-playbook /opt/plexguide/menu/version/missing_pull.yml; fi
 }
 
